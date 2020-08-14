b327b332b563432331fde210ad8c4ed517a1e913
ivine369 <ivinekemuma014@gmail.com>
2020-08-14 10:04:50 +0300

    add the media url to the url patterns

--- Pictures/urls.py
+++ Pictures/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-#url patterns
-urlpatterns = [
-    url(r'^$', views.picture_list,name='picture_list'),
+urlpatterns=[
+    url(r'^$',views.picture_list,name='picture_list'),
     url(r'^(?P<category_slug>[-\w]+)/$', views.picture_list,name='picture_list_by_category'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.picture_detail,name='picture_detail'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
