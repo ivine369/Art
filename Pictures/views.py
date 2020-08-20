ff9fe2349ac9012a9e40f95c18d1a1a0722ed963
ivine369 <ivinekemuma014@gmail.com>
2020-08-20 10:27:45 +0300

    make changes in files

--- Pictures/views.py
+++ Pictures/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Category, Picture
-from django.http import HttpResponse
 
 # Create your views here.
-def picture_list(request):
+def picture_list(request, category_slug=None):
+    category = None
     categories = Category.objects.all()
     pictures = Picture.objects.filter()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        pictures = pictures.filter(category=category)
+    return render(request, 'pictures/picture/list.html',{'category': category,
+                   'categories': categories,
+                   'pictures': pictures})
 
 
-def picture_detail(request, id):
-    picture=Picture.objects.get(id=id)
-    return render(request,'Pictures/picture/detail.html',{'picture': picture})
-    return render(request, 'list.html',{'categories': categories,'pictures': pictures})
+def picture_detail(request, id, slug):
+    picture = get_object_or_404(Picture,id=id,slug=slug)
+    return render(request,'pictures/picture/detail.html',{'picture': picture})
 
-    return render(request, 'auth_lifecycle/user_profile.html',
-           context_instance=RequestContext(request))
+# def search_results(request):
+#     if 'picture' in request.GET and request.GET['picture']:
+#         search_input = request.GET.get('image')
+#         searched_images = Image.search_by_category(search_input)
+#         message = f"{search_input}"
+
+#         return render(request, 'search.html', {"message":message, "images":searched_images})
+
+#     else:
+#         message = "Please input something in the search field"
+#         return render(request, 'search.html', {'message':message})
